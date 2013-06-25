ff36eb2db6b28fb24323dfb7f99abeb4def5aee2
Piotr Walczyszyn <piotr.walczyszyn@gmail.com>
2013-06-25 12:01:25 +0200

    cleanups

--- js/index.js
+++ js/index.js
@@ -12,87 +12,89 @@
             }
         };
         // On desktop don't have to wait for anything
-        onDeviceReady(true);
+        onDeviceReady();
     }
-
 
     function onDeviceReady() {
 
         // Initializing Parse API's
-        Parse.initialize("DeE1IIk6SSWxDVAiywycW78jUBA4ZXXT1nZrFfoV", "QsKQMMV9tQLMiO9GfSh305qP6cy3gqfqCTSQyFEP");
+        Parse.initialize('DeE1IIk6SSWxDVAiywycW78jUBA4ZXXT1nZrFfoV', 'QsKQMMV9tQLMiO9GfSh305qP6cy3gqfqCTSQyFEP');
 
-        findUsersNearby();
+        // Getting user coords using HTML5 geo API
+        getCoords();
+
+    }
+
+    function getCoords() {
+
+        // Using GeoLocation API
+        navigator.geolocation.getCurrentPosition(function (position) {
+            // Recursive call with coords this time
+            findUsersNearby(position.coords);
+        }, function (error) {
+            // Something went wrong, showing alert box
+            navigator.notification.alert('Could\'t obtain your location please try again!', null, 'Error');
+        }, {
+            maximumAge: 3000,
+            timeout: 5000,
+            enableHighAccuracy: true
+        });
 
     }
 
     function findUsersNearby(coords) {
-        if (typeof coords === 'undefined') {
 
-            // Using GeoLocation API
-            navigator.geolocation.getCurrentPosition(function (position) {
-                // Recursive call with coords this time
-                findUsersNearby(position.coords);
-            }, function (error) {
-                // Something went wrong, showing alert box
-                navigator.notification.alert('Could\'t obtain your location please try again!', null, 'Error');
-            }, {
-                maximumAge: 3000,
-                timeout: 5000,
-                enableHighAccuracy: true
-            });
+        // Defining UserLocation type
+        var UserLocation = Parse.Object.extend('UserLocation');
 
-        } else {
-            // Defining UserLocation type
-            var UserLocation = Parse.Object.extend('UserLocation');
+        // Creating a Parse query
+        var locQuery = new Parse.Query(UserLocation);
 
-            // Creating a Parse query
-            var locQuery = new Parse.Query(UserLocation);
+        // Searching for checkedin users in 100 meters distance
+        locQuery.withinKilometers('coords',
+            new Parse.GeoPoint({
+            latitude: coords.latitude,
+            longitude: coords.longitude
+        }),
+            0.1 // 100 meters range
+        );
 
-            // Searching for checkedin users in 100 meters distance
-            locQuery.withinKilometers('coords',
-                new Parse.GeoPoint({
-                latitude: coords.latitude,
-                longitude: coords.longitude
-            }),
-                0.1 // 100 meters range
-            );
+        // Returning results array should include referenced User object
+        locQuery.include("user");
 
-            // Returning results array should include referenced User object
-            locQuery.include("user");
-            // Results should be sorted descending by createdAt date
-            locQuery.descending("createdAt");
+        // Results should be sorted descending by createdAt date
+        locQuery.descending("createdAt");
 
-            // Execute the query
-            locQuery.find({
-                success: function (results) {
+        // Execute the query
+        locQuery.find({
+            success: function (results) {
 
-                    // Iterate through the results
-                    var items = [];
-                    results.forEach(function (userLocation) {
+                // Iterate through the results
+                var items = [];
+                results.forEach(function (userLocation) {
 
-                        var user = userLocation.get('user');
+                    var user = userLocation.get('user');
 
-                        if (user) {
-                            // Creating new list item
-                            var item = _.template($('#user-list-item').html(), {
-                                user: user,
-                                userLocation: userLocation
-                            });
-                            
-                            items.push(item);
-                        }
+                    if (user) {
+                        // Creating new list item
+                        var item = _.template($('#user-list-item').html(), {
+                            user: user,
+                            userLocation: userLocation
+                        });
 
-                    });
+                        items.push(item);
+                    }
 
-                    $('.topcoat-list').html(items);
+                });
 
-                },
-                error: function (error) {
-                    navigator.notification.alert('error ' + error.message + ' code: ' + error.code, null, 'Error');
-                }
-            });
-        }
+                $('.topcoat-list').html(items);
+
+            },
+            error: function (error) {
+                navigator.notification.alert('error ' + error.message + ' code: ' + error.code, null, 'Error');
+            }
+        });
+
     }
-
 
 })();
